91c6ff33f6f28af4219843119232f00a121dd63a
Edgar Snow <edgarsnow572000@gmail.com>
2023-04-28 01:10:07 +0630

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,12 @@ let cpuUsageInterval;
 const cpuModel = os.cpus()[0].model;
 const cpuCores = os.cpus().length / 2;
 
-// This function searches Google for the TDP of the CPU model.
+/**
+ * This function searches Google for the TDP of the CPU model.
+ *
+ * @param {string} cpuModel - The model of the CPU.
+ * @returns {Promise<number>} - The TDP of the CPU model.
+ */
 async function searchCPU(cpuModel) {
 
     // The search query.
@@ -91,25 +96,29 @@ processes = taskListOutput
 
 // Clear the console and move the cursor to the top-left corner
 function clearConsole() {
-    console.clear();
-    readline.cursorTo(rl.output, 0, 0);
-    readline.clearScreenDown(rl.output);
+    console.clear(); // Clear the console window
+    readline.cursorTo(rl.output, 0, 0); // Move the cursor to the top-left corner of the console window
+    readline.clearScreenDown(rl.output); // Clear the screen below the cursor
 }
+
 
 // Print the list of running processes to the console
 function printProcesses(searchQuery = '') {
-    clearConsole();
-    console.log('GreenPower');
-    console.log('Build: 0.0.1');
-    console.log(`CPU Model: ${cpuModel} \nCPU Cores: ${cpuCores}`);
-    console.log(`Search: ${searchQuery}`);
-    console.log('List of running processes:\n');
-    console.log("   Image Name                         PID     Session Name    Session Number  MemUsage");
+    clearConsole(); // Clear the console window and move the cursor to the top-left corner
+    console.log('GreenPower'); // Log the name of the program to the console
+    console.log('Build: 0.0.1'); // Log the version number of the program to the console
+    console.log(`CPU Model: ${cpuModel} \nCPU Cores: ${cpuCores}`); // Log the CPU model and number of cores to the console
+    console.log(`Search: ${searchQuery}`); // Log the search query to the console
+    console.log('List of running processes:\n'); // Log a header for the list of running processes to the console
+    console.log("   Image Name                         PID     Session Name    Session Number  MemUsage"); // Log a header for the table of running processes to the console
     console.log("------------------------------------------------------------------------------------------");
 
+    // Filter the list of running processes based on the search query
     filteredProcesses = searchQuery !== ''
         ? processes.filter(p => p.imageName.toLowerCase().includes(searchQuery.toLowerCase()))
         : processes;
+
+    // Loop through the filtered list of running processes and log each one to the console
     for (let i = startIndex; i < Math.min(startIndex + 18, filteredProcesses.length); i++) {
         const process = filteredProcesses[i];
         const isSelected = i === selectedIndex;
@@ -120,18 +129,18 @@ function printProcesses(searchQuery = '') {
 
 //Track the CPU usage of the selected process and calculate the power consumption and average power consumption per sec using it
 async function startCpuUsageTracking() {
-    const selectedProcess = filteredProcesses[selectedIndex];
-    const tdp = await searchCPU(cpuModel) || 65;
-    const startTime = new Date();
-    let duration = 0;
-    let totalPowerConsumption = 0;
-    const cpuUsageCommand = `wmic path Win32_PerfFormattedData_PerfProc_Process where "IDProcess=${selectedProcess.pid}" get PercentProcessorTime`;
+    const selectedProcess = filteredProcesses[selectedIndex]; // Get the currently selected process
+    const tdp = await searchCPU(cpuModel) || 65; // Get the TDP of the CPU model, default to 65 if it is not found
+    const startTime = new Date(); // Get the current time
+    let duration = 0; // Initialize the duration of the tracking to 0
+    let totalPowerConsumption = 0; // Initialize the total power consumption to 0
+    const cpuUsageCommand = `wmic path Win32_PerfFormattedData_PerfProc_Process where "IDProcess=${selectedProcess.pid}" get PercentProcessorTime`; // Build the command to get the CPU usage for the selected process
     cpuUsageInterval = setInterval(() => {
         try {
-            const output = execSync(cpuUsageCommand).toString();
-            const cpuUsage = parseFloat(output.trim().split('\r\n')[1]);
-            const powerConsumption = parseFloat(cpuUsage) * tdp / 100;
-            duration = Math.round((new Date() - startTime) / 1000);
+            const output = execSync(cpuUsageCommand).toString(); // Execute the CPU usage command and convert the output to a string
+            const cpuUsage = parseFloat(output.trim().split('\r\n')[1]); // Parse the CPU usage value from the output
+            const powerConsumption = parseFloat(cpuUsage) * tdp / 100; // Calculate the power consumption based on the CPU usage and TDP
+            duration = Math.round((new Date() - startTime) / 1000); // Calculate the duration of the tracking in seconds
             totalPowerConsumption += powerConsumption;
             const avgPowerConsumption = totalPowerConsumption / duration;
             clearConsole();
@@ -158,13 +167,14 @@ async function startCpuUsageTracking() {
 //Readlines to allow users to input search queries and arrow keys to navigate the list
 rl.question('Enter search query (press enter to skip): ', (searchQuery) => {
     printProcesses(searchQuery);
-
+    // Set the input mode to raw so that we can capture arrow key events
     rl.input.setRawMode(true);
     rl.input.on('keypress', (_, key) => {
+        // Check for the "Ctrl+C" key combination to exit the program
         if (key.name === 'c' && key.ctrl) {
             rl.close();
             process.exit();
-        } else if (key.name === 'up' && selectedIndex > startIndex) {
+        } else if (key.name === 'up' && selectedIndex > startIndex) {// Listens for the "up" key to move the selected process
 
             selectedIndex--;
             if (selectedIndex === startIndex + 4 && startIndex > 0) {
@@ -172,19 +182,19 @@ rl.question('Enter search query (press enter to skip): ', (searchQuery) => {
                 printProcesses(searchQuery);
             }
             printProcesses(searchQuery);
-        } else if (key.name === 'down' && selectedIndex < processes.length - 1) {
+        } else if (key.name === 'down' && selectedIndex < processes.length - 1) {// Listens for the "down" key to move the selected process
             selectedIndex++;
             if (selectedIndex === startIndex + 10 && startIndex + 10 < processes.length) {
                 startIndex++;
                 printProcesses(searchQuery);
             }
             printProcesses(searchQuery);
-        } else if (key.name === 'return') {
+        } else if (key.name === 'return') {// Listens for the "return/enter" key to finalize the selected process
             const selectedProcess = filteredProcesses[selectedIndex];
             console.log(`Starting CPU usage tracking for process '${selectedProcess.imageName}'(PID: ${selectedProcess.pid})`);
             clearInterval(cpuUsageInterval);
             startCpuUsageTracking();
-        } else if (key.name === 'escape') {
+        } else if (key.name === 'escape') {// Listens for the "escape/esc" key to stop tthe program
             clearInterval(cpuUsageInterval);
             console.log(`Average Power Consumption: ${lastKnownPowerConsumption.toFixed(2)}W`);
             if (lastKnownPowerConsumption >= 70) {
@@ -195,6 +205,7 @@ rl.question('Enter search query (press enter to skip): ', (searchQuery) => {
                 console.log('Power Consumption is Low');
             }
             console.log('Exiting process monitor...');
+            console.log('Thank you for using our program!');
             if (cpuUsageInterval) {
                 clearInterval(cpuUsageInterval);
             }
